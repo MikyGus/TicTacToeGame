125c45068bc037b53a816cc4b793d788ffb3bf55
MikyGus <mikygus@gmail.com>
2023-11-10 09:57:41 +0100

    Add a StateMachine to handle PlayerTurns

---
+++ TicTacToe/PlayerTurnEngine.cs
@@ -0,0 +1,30 @@
+﻿using TicTacToe.Abstract;
+
+namespace TicTacToe;
+internal class PlayerTurnEngine
+{
+    private readonly Queue<IPlayer> _playerQueue = new();
+    public PlayerTurnEngine(IEnumerable<IPlayer> players)
+    {
+        foreach (IPlayer p in players) 
+            if (p is not null) 
+                _playerQueue.Enqueue(p);
+            else
+                throw new ArgumentNullException(nameof(p));
+        if (_playerQueue.Count < 2)
+            throw new ArgumentException($"Need a minimum of two players, have only {_playerQueue.Count}");
+    }
+    public int PlayerCount() => _playerQueue.Count;
+    public IPlayer CurrentPlayer => _playerQueue.Peek();
+    public IEnumerable<IPlayer> Players()
+    {
+        foreach(IPlayer p in _playerQueue)
+            yield return p;
+    }
+    public IPlayer NextPlayer()
+    {
+        var player = _playerQueue.Dequeue();
+        _playerQueue.Enqueue(player);
+        return CurrentPlayer;
+    }
+}

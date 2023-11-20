6e8e44232bc951a9b4fd3a6ef92fd976497cb9e2
MikyGus <mikygus@gmail.com>
2023-11-20 22:23:50 +0100

    Add TopRow for winnerState

--- TicTacToe/States/PlayerWonState.cs
+++ TicTacToe/States/PlayerWonState.cs
@@ -1,17 +1,24 @@
 ﻿using TicTacToe.Abstract;
+using TicTacToe.Models;
+using TicTacToe.Renderers;
 
 namespace TicTacToe.States;
 internal class PlayerWonState : IEngineState
 {
-    public PlayerWonState()
+    private readonly IPlayer _player;
+
+    public PlayerWonState(IPlayer player)
     {
-        
+        _player = player;
     }
 
     public void Activate()
     {
-        Console.SetCursorPosition(20, 0);
-        Console.WriteLine("We have a winner!!!!!!!!!!");
+        var widthOfRow = 90;
+        var barToRender = String.Format("[{0}]-{1} <<--- Is the winner!!!!!!! Press any key to return to main menu", _player.Sprite, _player.Name);
+        barToRender = barToRender[..(widthOfRow < barToRender.Length ? widthOfRow : barToRender.Length)].PadRight(widthOfRow);
+
+        ConsoleDraw.WriteAtPosition(new Vector2(0, 0), barToRender, ConsoleColor.Black, _player.SpriteColor);
     }
 
     public void Deactivate()
@@ -24,5 +31,6 @@ internal class PlayerWonState : IEngineState
 
     public void ProcessInput(ConsoleKeyInfo key)
     {
+        Program.GameEngine.SwitchState(new MainMenuState());
     }
 }

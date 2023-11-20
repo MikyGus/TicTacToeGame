84e6dbfbc2c1531d223442e19dba23150a20e665
MikyGus <mikygus@gmail.com>
2023-11-20 15:56:09 +0100

    BugFix NullException when accesing Winconditions

--- TicTacToe/Settings/ConfigData.cs
+++ TicTacToe/Settings/ConfigData.cs
@@ -7,7 +7,7 @@ internal class ConfigData
 {
     public GridData Grid { get; set; } = new();
     public PlayerData Player { get; set; } = new();
-    public WinConditionsData WinConditions { get; set; }
+    public WinConditionsData WinConditions { get; set; } = new();
 
     public ConfigData Duplicate() => new()
     {
